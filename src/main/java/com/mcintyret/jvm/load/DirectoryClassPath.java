67f82c8d3aa9eb255185d771a7bf079f120b7301
Tom McIntyre <tom.mcintyre5@gmail.com>
2014-05-20 22:41:07 +0100

    DirectoryClassPath can read recursively

--- src/main/java/com/mcintyret/jvm/load/DirectoryClassPath.java
+++ src/main/java/com/mcintyret/jvm/load/DirectoryClassPath.java
@@ -17,26 +17,42 @@ public class DirectoryClassPath implements ClassPath {
 
     private final Path path;
 
+    private final boolean recursive;
+
     public DirectoryClassPath(String path) {
-        this(Paths.get(path));
+        this(path, true);
     }
 
     public DirectoryClassPath(File dir) {
-        this(dir.toPath());
+        this(dir, true);
     }
 
     public DirectoryClassPath(Path path) {
+        this(path, true);
+    }
+
+    public DirectoryClassPath(String path, boolean recursive) {
+        this(Paths.get(path), recursive);
+    }
+
+    public DirectoryClassPath(File dir, boolean recursive) {
+        this(dir.toPath(), recursive);
+    }
+
+    public DirectoryClassPath(Path path, boolean recursive) {
         if (!Files.isDirectory(path)) {
             throw new IllegalArgumentException("Should be a Directory!");
         }
         this.path = path;
+        this.recursive = recursive;
     }
 
 
     @Override
     public Iterable<InputStream> getClassFileStreams() throws IOException {
+        int depth = recursive ? Integer.MAX_VALUE : 1;
         final List<InputStream> list = new ArrayList<>();
-        Files.walkFileTree(path, Collections.emptySet(), 1, new SimpleFileVisitor<Path>() {
+        Files.walkFileTree(path, Collections.emptySet(), depth, new SimpleFileVisitor<Path>() {
 
             @Override
             public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
