dd4517a2495bd956da84f97d63e51df56ae1c48c
Tom McIntyre <tom.mcintyre5@gmail.com>
2015-12-24 12:49:51 +0000

    Jvm: fix main class name and install print native method

--- core/src/main/java/com/mcintyret/jvm/Jvm.java
+++ core/src/main/java/com/mcintyret/jvm/Jvm.java
@@ -1,5 +1,12 @@
 package com.mcintyret.jvm;
 
+import com.mcintyret.jvm.core.Heap;
+import com.mcintyret.jvm.core.exec.OperationContext;
+import com.mcintyret.jvm.core.nativeimpls.NativeImplementationAdapter;
+import com.mcintyret.jvm.core.nativeimpls.NativeImplementationRegistry;
+import com.mcintyret.jvm.core.nativeimpls.NativeReturn;
+import com.mcintyret.jvm.core.type.MethodSignature;
+import com.mcintyret.jvm.core.util.Utils;
 import com.mcintyret.jvm.load.AggregatingClassPath;
 import com.mcintyret.jvm.load.ClassPath;
 import com.mcintyret.jvm.load.DirectoryClassPath;
@@ -15,7 +22,7 @@ public class Jvm {
         if (args.length < 1) {
             throw new IllegalArgumentException("No Main class given");
         }
-        String mainClass = args[0];
+        String mainClass = args[0].replaceAll("\\.", "/");
         String[] theArgs = new String[args.length - 1];
         if (theArgs.length > 0) {
             System.arraycopy(args, 1, theArgs, 0, theArgs.length);
@@ -40,6 +47,20 @@ public class Jvm {
             }
         }
 
+        installNativePrintMethod(mainClass);
+
         new Runner().run(classPath, mainClass);
     }
+
+    private static void installNativePrintMethod(String mainClass) {
+        NativeImplementationRegistry.registerNative(new NativeImplementationAdapter(mainClass, MethodSignature.parse("print", "(Ljava/lang/String;)V")) {
+            @Override
+            public NativeReturn execute(int[] args, OperationContext ctx) {
+                System.out.println("NATIVE METHOD!!!: " + Utils.toString(Heap.getOopClass(args[0])));
+                return NativeReturn.forVoid();
+            }
+        });
+    }
+
+
 }
