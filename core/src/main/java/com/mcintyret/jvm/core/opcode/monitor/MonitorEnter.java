11aa7e39f9cc505c6c2cdca4b1f22b38f8f61f8f
Tom McIntyre <tom.mcintyre5@gmail.com>
2015-12-28 10:59:30 +0000

    MonitorEnter: waiting for the lock acts as a safe point for GC

--- core/src/main/java/com/mcintyret/jvm/core/opcode/monitor/MonitorEnter.java
+++ core/src/main/java/com/mcintyret/jvm/core/opcode/monitor/MonitorEnter.java
@@ -1,12 +1,18 @@
 package com.mcintyret.jvm.core.opcode.monitor;
 
+import com.mcintyret.jvm.core.Heap;
+
 import java.util.concurrent.locks.Lock;
 
 class MonitorEnter extends Monitor {
 
     @Override
     protected void handleMonitor(Lock monitor) {
-        monitor.lock();
+        if (!monitor.tryLock()) {
+            Heap.threadSleeping();
+            monitor.lock();
+            Heap.threadWaking();
+        }
     }
 
     @Override
