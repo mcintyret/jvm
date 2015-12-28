26baf5fb3868056afcdbddf265a6437e202eb738
Tom McIntyre <tom.mcintyre5@gmail.com>
2015-12-28 22:45:48 +0000

    Dup2_X1: fix bug

--- core/src/main/java/com/mcintyret/jvm/core/opcode/dup/Dup2_X1.java
+++ core/src/main/java/com/mcintyret/jvm/core/opcode/dup/Dup2_X1.java
@@ -1,6 +1,7 @@
 package com.mcintyret.jvm.core.opcode.dup;
 
 import com.mcintyret.jvm.core.exec.OperationContext;
+import com.mcintyret.jvm.core.exec.Variable;
 import com.mcintyret.jvm.core.exec.VariableStack;
 import com.mcintyret.jvm.core.exec.WideVariable;
 import com.mcintyret.jvm.core.opcode.OpCode;
@@ -11,16 +12,36 @@ class Dup2_X1 extends OpCode {
     public void execute(OperationContext ctx) {
         VariableStack stack = ctx.getStack();
 
-        WideVariable top = stack.popWide();
-        WideVariable next = stack.popWide();
+        if (stack.peekType().isDoubleWidth()) {
+            WideVariable top = stack.popWide();
+            Variable next = popSingleWidth(stack);
 
-        stack.pushWide(top);
-        stack.pushWide(next);
-        stack.pushWide(top);
+            stack.pushWide(top);
+            stack.push(next);
+            stack.pushWide(top);
+        } else {
+            Variable one = popSingleWidth(stack);
+            Variable two = popSingleWidth(stack);
+            Variable three = popSingleWidth(stack);
+
+            stack.push(two);
+            stack.push(one);
+            stack.push(three);
+            stack.push(two);
+            stack.push(one);
+        }
     }
 
     @Override
     public byte getByte() {
         return 0x5D;
+    }
+
+    private Variable popSingleWidth(VariableStack stack) {
+        Variable v = stack.pop();
+        if (v.getType().isDoubleWidth()) {
+            throw new AssertionError();
+        }
+        return v;
     }
 }
