b84150659519afc7a1d2efc4ebc4f2a598b6dead
Tom McIntyre <tom.mcintyre5@gmail.com>
2015-12-29 09:05:18 +0000

    ClassLoaderNatives: implement findLoadedClass

--- core/src/main/java/com/mcintyret/jvm/core/nativeimpls/ClassLoaderNatives.java
+++ core/src/main/java/com/mcintyret/jvm/core/nativeimpls/ClassLoaderNatives.java
@@ -1,14 +1,28 @@
 package com.mcintyret.jvm.core.nativeimpls;
 
+import com.mcintyret.jvm.core.clazz.ClassObject;
 import com.mcintyret.jvm.core.exec.OperationContext;
 import com.mcintyret.jvm.core.exec.Variables;
+import com.mcintyret.jvm.core.oop.OopClass;
 import com.mcintyret.jvm.core.type.MethodSignature;
+import com.mcintyret.jvm.core.util.Utils;
+
+import static com.mcintyret.jvm.load.ClassLoader.getDefaultClassLoader;
 
 public enum ClassLoaderNatives implements NativeImplementation {
     REGISTER_NATIVES("registerNatives", "()V") {
         @Override
         public NativeReturn execute(Variables args, OperationContext ctx) {
             return NativeReturn.forVoid();
+        }
+    },
+    FIND_LOADED_CLASS0("findLoadedClass0", "(Ljava/lang/String;)Ljava/lang/Class;") {
+        @Override
+        public NativeReturn execute(Variables args, OperationContext ctx) {
+            String className = Utils.toJvmClassName(Utils.toString((OopClass) args.getOop(1)));
+
+            ClassObject co = getDefaultClassLoader().getClassObject(className);
+            return NativeReturn.forReference(co.getOop());
         }
     };
 
