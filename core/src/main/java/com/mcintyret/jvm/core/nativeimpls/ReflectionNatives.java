a0504796df5f8dd553806164a3d0ed11338ea5b1
Tom McIntyre <tom.mcintyre5@gmail.com>
2015-12-29 01:52:34 +0000

    ReflectionNatives: proper implementation of getCallerClass

--- core/src/main/java/com/mcintyret/jvm/core/nativeimpls/ReflectionNatives.java
+++ core/src/main/java/com/mcintyret/jvm/core/nativeimpls/ReflectionNatives.java
@@ -1,13 +1,15 @@
 package com.mcintyret.jvm.core.nativeimpls;
 
+import com.mcintyret.jvm.core.exec.Execution;
 import com.mcintyret.jvm.core.exec.OperationContext;
 import com.mcintyret.jvm.core.exec.Variables;
 import com.mcintyret.jvm.core.oop.OopClassClass;
 import com.mcintyret.jvm.core.type.MethodSignature;
-import com.mcintyret.jvm.core.type.NonArrayType;
 import com.mcintyret.jvm.core.type.ReferenceType;
 import com.mcintyret.jvm.core.type.Type;
 import com.mcintyret.jvm.parse.Modifier;
+
+import java.util.Iterator;
 
 /**
  * User: tommcintyre
@@ -19,7 +21,8 @@ public enum ReflectionNatives implements NativeImplementation {
         public NativeReturn execute(Variables args, OperationContext ctx) {
             // Literally no documentation in the world about what this no-arg form is.
             // TODO: although I could make an educated guess...
-            return NativeReturn.forReference(NonArrayType.forClass("java/lang/Object").getOopClassClass());
+            Execution callerExecution = getSecond(ctx.getExecutionStack().getStack().iterator());
+            return NativeReturn.forReference(callerExecution.getMethod().getClassObject().getOop());
         }
     },
     GET_CLASS_ACCESS_FLAGS("getClassAccessFlags", "(Ljava/lang/Class;)I") {
@@ -51,5 +54,10 @@ public enum ReflectionNatives implements NativeImplementation {
     @Override
     public MethodSignature getMethodSignature() {
         return methodSignature;
+    }
+
+    private static <T> T getSecond(Iterator<T> it) {
+        it.next();
+        return it.next();
     }
 }
